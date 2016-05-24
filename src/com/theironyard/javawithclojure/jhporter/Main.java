2098d120f5a0bf81aaeeeeeef3a66700c50128e4
jeffryporter <jporter7@gmail.com>
2016-05-24 11:23:44 -0400

    Add factory method

--- src/com/theironyard/javawithclojure/jhporter/Main.java
+++ src/com/theironyard/javawithclojure/jhporter/Main.java
@@ -5,9 +5,9 @@ public class Main
 
     public static void main(String[] args)
     {
-	    Dog d = new Dog();
-        Snake s = new Snake();
-        Hawk h = new Hawk();
+	    Animal d = createAnimal("Dog");
+        Animal s = createAnimal("Snake");
+        Animal h = createAnimal("Hawk");
 
         d.makeSound();
         s.makeSound();
@@ -16,6 +16,20 @@ public class Main
         System.out.println(d);
         System.out.println(s);
         System.out.println(h);
+    }
 
+    public static Animal createAnimal(String name)
+    {
+        switch (name)
+        {
+            case "Dog":
+                return new Dog();
+            case "Snake":
+                return new Snake();
+            case "Hawk":
+                return new Hawk();
+
+        }
+        return new Animal();
     }
 }
